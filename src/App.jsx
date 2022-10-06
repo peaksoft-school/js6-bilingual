d6b2262df63a8eda71f15c1fda3ef6a1bfd0e9d3
necxodnaya <109329007+necxodnaya@users.noreply.github.com>
2022-10-06 14:44:48 +0600

    added modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import Modal from "components/UI/Modal";
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            App
+            <Modal />
+        </div>
+    );
 };
 
 export default App;
