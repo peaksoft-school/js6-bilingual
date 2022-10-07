0142f596badee7c945726dba6f1efe37321a9ff2
Chika <chyngyzalkarbekov@gmail.com>
2022-10-07 12:38:15 +0600

    added radio button

--- src/components/UI/RadioButton.jsx
+++ src/components/UI/RadioButton.jsx
@@ -5,12 +5,12 @@ import Radio from "@mui/material/Radio";
 import { styled } from "@mui/material/styles";
 
 function RadioButton({ onChange, value }) {
-    return <StyledRadio onChange={onChange} value={value} />;
+    return <StyleRadio onChange={onChange} value={value} />;
 }
 
 export default RadioButton;
 
-const StyledRadio = styled(Radio)`
+const StyleRadio = styled(Radio)`
     &.MuiButtonBase-root {
         width: 18px;
         height: 18px;
