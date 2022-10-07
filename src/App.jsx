60c6e9b1a186d7f9e4122024a1a1a974358d2644
necxodnaya <109329007+necxodnaya@users.noreply.github.com>
2022-10-07 18:31:53 +0600

    added modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
 import React from "react";
 
-import BasicModal from "components/UI/Modal";
-
 const App = () => {
-    return (
-        <BasicModal open>
-            <div>
-                privet ya, ldkfgjoifhjorfnbksdfhg98w4, hgokldfngb9wrthgd,fmbosdfuhwrmhlwrkj
-                gwrjhwrltmwrthojwrtml;wrjhwprot;rtjrekltjeorterlktherothjeorjdora
-            </div>
-            <div>
-                privet ya, ldkfgjoifhjorfnbksdfhg98w4, hgokldfngb9wrthgd,fmbosdfuhwrmhlwrkj
-                gwrjhwrltmwrthojwrtml;wrjhwprot;rtjrekltjeorterlktherothjeorjdora
-            </div>
-            <div>
-                privet ya, ldkfgjoifhjorfnbksdfhg98w4, hgokldfngb9wrthgd,fmbosdfuhwrmhlwrkj
-                gwrjhwrltmwrthojwrtml;wrjhwprot;rtjrekltjeorterlktherothjeorjdora
-            </div>
-            <div>
-                privet ya, ldkfgjoifhjorfnbksdfhg98w4, hgokldfngb9wrthgd,fmbosdfuhwrmhlwrkj
-                gwrjhwrltmwrthojwrtml;wrjhwprot;rtjrekltjeorterlktherothjeorjdora
-            </div>
-        </BasicModal>
-    );
+    return <div>App</div>;
 };
 
 export default App;
