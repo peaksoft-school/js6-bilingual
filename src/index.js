7b4187847234092601e6f096693c28061b01a232
emirlan <emirlanjumaev16@gmail.com>
2022-10-11 19:53:53 +0600

    deleted unnesessary files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
+
+import PopUp from "components/UI/PopUp";
+
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
+import "react-notifications-component/dist/theme.css";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+root.render(<App />);
