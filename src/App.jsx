3b877dee29da3c1b2f71d578e5f26075f0442b32
emirlan <emirlanjumaev16@gmail.com>
2022-10-14 18:29:09 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Input from "components/UI/Input";
+
 const App = () => {
-    return <div>App</div>;
+    const [value, setValue] = React.useState("");
+    React.useEffect(() => {
+        console.log(value);
+    }, [value]);
+    return (
+        <div>
+            <Input setValue={setValue} value={value} forInput={{ label: "Name", size: "sm" }} />
+        </div>
+    );
 };
 
 export default App;
