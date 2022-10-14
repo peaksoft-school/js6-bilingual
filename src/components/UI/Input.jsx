3eed21c23bf2177579fe5ec6f5d0b7502dc5ce66
emirlan <emirlanjumaev16@gmail.com>
2022-10-14 18:22:17 +0600

    fix input

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -2,34 +2,56 @@ import React from "react";
 
 import { TextField } from "@mui/material";
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const Input = ({ forInput, colorText = "gray", colorLabelTextAndBorderAndHover = "#3A10E5" }) => {
-    // FOR INPUT colorLabelTextAndBorderAndHover
-
-    const CssTextField = styled(TextField)({
-        "& input": {
-            color: colorText,
-        },
-        "& label.Mui-focused": {
-            color: colorLabelTextAndBorderAndHover,
-        },
-        "& .MuiInput-underline:after": {
-            borderBottomColor: "green",
-        },
-        "& .MuiOutlinedInput-root": {
-            "& fieldset": {
-                borderColor: "#BDBDBD",
-            },
-            "&:hover fieldset": {
-                borderColor: colorLabelTextAndBorderAndHover,
-            },
-            "&.Mui-focused fieldset": {
-                borderColor: colorLabelTextAndBorderAndHover,
-            },
-        },
-    });
-    return <CssTextField {...forInput} fullWidth id="custom-css-outlined-input" />;
+const Input = ({
+    forInput,
+    colorеext = "gray",
+    colorlabeltextandborderandhover = "#3A10E5",
+    value,
+    setValue,
+}) => {
+    // FOR INPUT colorlabeltextandborderandhover
+    return (
+        <CssTextField
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+            colorеext={colorеext}
+            colorlabeltextandborderandhover={colorlabeltextandborderandhover}
+            {...forInput}
+            fullWidth
+            id="custom-css-outlined-input"
+        />
+    );
 };
+
+const CssTextField = styled(TextField)`
+    ${(props) => {
+        console.log(props);
+        return css`
+    & input: {
+        color: ${props.colorеext},
+    },
+    & label.Mui-focused: {
+        color: ${props.colorlabeltextandborderandhover},
+    },
+    & .MuiInput-underline:after: {
+        borderBottomColor: green,
+    },
+    & .MuiOutlinedInput-root: {
+        & fieldset: {
+            borderColor: #BDBDBD,
+        },
+        &:hover fieldset: {
+            borderColor: ${props.colorlabeltextandborderandhover},
+        },
+        &.Mui-focused fieldset: {
+            borderColor: ${props.colorlabeltextandborderandhover},
+        },
+    },
+    
+    `;
+    }}
+`;
 
 export default Input;
