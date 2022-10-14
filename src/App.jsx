0d30ba8d315c0b8644995d1e8a4e81bab330c615
necxodnaya <109329007+necxodnaya@users.noreply.github.com>
2022-10-14 15:32:30 +0600

    fixed/AppClear

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 
-<<<<<<< HEAD
-import CustomizedSwitches from "components/UI/Switcher";
-=======
-import ImagePicker from "components/UI/ImagePicker";
->>>>>>> a62b621f1905e2b9c3be6d611931de3019fa7d7d
-
 const App = () => {
-    return (
-        <div>
-<<<<<<< HEAD
-            <CustomizedSwitches />
-=======
-            <ImagePicker getImages={(img) => console.log(img)} />
->>>>>>> a62b621f1905e2b9c3be6d611931de3019fa7d7d
-        </div>
-    );
+    return <div>App</div>;
 };
 
 export default App;
