76333cef5832ec7e3d361005113342367d08d7cc
Chika <chyngyzalkarbekov@gmail.com>
2022-10-15 16:35:01 +0600

    clear App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
-import UiCard from "components/UI/UiCard";
-
 const App = () => {
-    return (
-        <div>
-            <UiCard>App</UiCard>
-        </div>
-    );
+    return <div>App</div>;
 };
 
 export default App;
