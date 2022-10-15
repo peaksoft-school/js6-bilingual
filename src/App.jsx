4a7a4676c5d6a7c0a0f4ad86d8fa981f62661d12
emirlan <emirlanjumaev16@gmail.com>
2022-10-15 12:18:16 +0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { Add } from "@mui/icons-material";
+import ButtonStyled from "components/UI/Button";
+
 const App = () => {
-    return <div>App</div>;
+    const someClick = () => {
+        console.log("some");
+    };
+    return (
+        <ButtonStyled
+            text="Add new"
+            variant="contained"
+            color="success"
+            click={someClick}
+            icon={<Add fontSize="small" />}
+        />
+    );
 };
 
 export default App;
---
+++ src/assets/icons/index.js
@@ -0,0 +1,2 @@
+export { default as EditIcon } from "./EditIcon.svg";
+export { default as DeleteIcon } from "./DeleteIcon.svg";
