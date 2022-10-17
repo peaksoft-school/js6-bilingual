62c8acad82fd5405d16f5a525bed2829508d18c2
emirlan <emirlanjumaev16@gmail.com>
2022-10-17 17:53:03 +0600

    go back

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-<<<<<<< HEAD
-import PopUp from "components/UI/PopUp";
-
-=======
->>>>>>> fc8e45b485f99e85489562e634a696641539abaa
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import "react-notifications-component/dist/theme.css";
@@ -13,4 +9,8 @@ import "react-notifications-component/dist/theme.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
