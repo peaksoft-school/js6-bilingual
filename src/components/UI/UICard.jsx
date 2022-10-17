d96ffd45c2a9b88a20b56287fd1282f704a4d7ea
Chika <chyngyzalkarbekov@gmail.com>
2022-10-17 16:38:18 +0600

    ui card

---
+++ src/components/UI/UICard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Card from "@mui/material/Card";
+
+import { styled } from "@mui/material/styles";
+
+function UICard({ children, cardwidth, cardheight, cardborderradius, cardboxshadow }) {
+    return (
+        <StyeldCard
+            cardwidth={cardwidth}
+            cardheight={cardheight}
+            cardborderradius={cardborderradius}
+            cardboxshadow={cardboxshadow}>
+            {children}
+        </StyeldCard>
+    );
+}
+
+export default UICard;
+
+const StyeldCard = styled(Card)`
+    width: ${(props) => props.cardwidth};
+    height: ${(props) => props.cardheight}
+    margin: 0 auto;
+    padding: 40px 43px;
+    box-shadow: ${(props) => props.cardboxshadow};
+    border-radius: ${(props) => props.cardborderradius};
+`;
