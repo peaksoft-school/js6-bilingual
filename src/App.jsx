d1ed0991142349989a539e163b75688e04dd5456
necxodnaya <109329007+necxodnaya@users.noreply.github.com>
2022-10-17 18:25:03 +0600

    added switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import SwitcherComp from "components/UI/Switcher";
 import "./App.css";
 
 const App = () => {
-    return <div className="app">App</div>;
+    return (
+        <div className="app">
+            <SwitcherComp />
+        </div>
+    );
 };
 
 export default App;
