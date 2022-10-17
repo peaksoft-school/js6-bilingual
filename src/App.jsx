b7fc4a6528e65885234e1c7473cda1680e3d3c2b
emirlan <emirlanjumaev16@gmail.com>
2022-10-17 19:03:42 +0600

    fixed conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,9 @@ import React from "react";
 import Input from "components/UI/Input";
 
 const App = () => {
-    const [value, setValue] = React.useState("");
-    React.useEffect(() => {
-        console.log(value);
-    }, [value]);
     return (
         <div>
-            <Input setValue={setValue} value={value} forInput={{ label: "Name", size: "sm" }} />
+            <Input handleChange={handleChange} forInput={{ label: "Name", size: "sm" }} />
         </div>
     );
 };
