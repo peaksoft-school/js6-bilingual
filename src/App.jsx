7cf8fa4668ff32f612d0d5921f2c0329207a892d
Chika <chyngyzalkarbekov@gmail.com>
2022-10-17 17:03:30 +0600

    ui card

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 
-import PopUp from "components/UI/PopUp";
-
 import "./App.css";
 
 const App = () => {
-    return (
-        <div className="app">
-            <PopUp titleIs="File saved " textIs="Successfully saved" typeIs="Success" />
-            <PopUp titleIs="Error" textIs="Please fill in all fields" typeIs="danger" />
-            <PopUp titleIs="Error" textIs="Please fill in all fields" typeIs="danger" />
-            <PopUp titleIs="Error" textIs="Please fill in all fields" typeIs="danger" />
-        </div>
-    );
+    return <div className="app">App</div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from "react";
 
-<<<<<<< HEAD
-import PopUp from "components/UI/PopUp";
-
-=======
->>>>>>> fc8e45b485f99e85489562e634a696641539abaa
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import "react-notifications-component/dist/theme.css";
 
 import App from "./App";
 
