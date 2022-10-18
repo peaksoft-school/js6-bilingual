1f1a042771495c43c2773c8af1717634b4aa9a80
elya <elka_kapus@mail.ru>
2022-10-18 18:53:33 +0600

    accordion-branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+import Accordion from "components/Accordion";
+
+const data = [
+    {
+        id: "a",
+        panel: "panel1",
+        question: "What is Your name?",
+        answer: "Elnura",
+    },
+    {
+        id: "b",
+        panel: "panel2",
+        question: "What is Your name?",
+        answer: "Elnura",
+    },
+    {
+        id: "c",
+        panel: "panel3",
+        question: "What is Your name?",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto eligendi provident dolorum et voluptatem totam recusandae dolorem? Recusandae consequuntur eveniet dolorum error ab quia odit. Sint vero optio cumque quasi?",
+    },
+];
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <Accordion data={data} />
+        </div>
+    );
 };
 
 export default App;
