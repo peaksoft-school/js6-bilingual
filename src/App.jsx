7061a7ce95a6c9ec286b044b7f299ae9f87428af
elya <elka_kapus@mail.ru>
2022-10-18 19:28:06 +0600

    accordion-footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
 import React from "react";
 
-<<<<<<< HEAD
-import Accordion from "components/Accordion";
-
-const data = [
-    {
-        id: "a",
-        panel: "panel1",
-        question: "What is Your name?",
-        answer: "Elnura",
-    },
-    {
-        id: "b",
-        panel: "panel2",
-        question: "What is Your name?",
-        answer: "Elnura",
-    },
-    {
-        id: "c",
-        panel: "panel3",
-        question: "What is Your name?",
-        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto eligendi provident dolorum et voluptatem totam recusandae dolorem? Recusandae consequuntur eveniet dolorum error ab quia odit. Sint vero optio cumque quasi?",
-    },
-];
-
-const App = () => {
-    return (
-        <div>
-            <Accordion data={data} />
-        </div>
-    );
-=======
 import "./App.css";
 
 const App = () => {
     return <div className="app">App</div>;
->>>>>>> 0fcc3fb0a11d772630b5f6b7f6fe5c403ca5c289
 };
 
 export default App;
