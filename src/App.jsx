31a9934ff51e09f5c83e7ba984e9f4a4cf50d6b6
Emirlan <emirlanjumaev16@gmail.com>
2022-10-27 00:41:00 +0600

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 
-import UseFullVideo from "components/UseFullVideo/UseFullVideo";
-
 import "./App.css";
 
 const App = () => {
-    return (
-        <div className="app">
-            <UseFullVideo />
-        </div>
-    );
+    return <div className="app">App</div>;
 };
 
 export default App;
