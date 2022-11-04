8a25d00d681d944be58c0bd9339481637e14f6f7
a6uxa4 <a6uxa4@icloud.com>
2022-11-04 18:47:26 +0600

    FixedHeaderBottomConflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 
 import "./App.css";
-import Header from "components/LandingPage/Header";
-
-import HeaderBottom from "components/LandingPage/HeaderBottom";
 
 const App = () => {
-    return (
-        <div className="app">
-            <HeaderBottom />
-        </div>
-    );
+    return <div className="app">App</div>;
 };
 
 export default App;
