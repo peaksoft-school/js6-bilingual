c1a7413cf31c5dadb3796268d65275cd1f70b019
a6uxa4 <a6uxa4@icloud.com>
2022-11-23 14:42:46 +0600

    save

--- src/components/UI/DataInput.jsx
+++ src/components/UI/DataInput.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 
-import TimeInput from "react-input-time";
+import styled from "styled-components";
 
-function DataInput() {
+function DataInput({ onChange, value }) {
     return (
-        <div>
-            <TimeInput class />
-        </div>
+        <StyledDataInput>
+            <StyledText>Duration (in minutes)</StyledText>
+            <StyledInput type="time" onChange={onChange} value={value} />
+        </StyledDataInput>
     );
 }
-
+const StyledDataInput = styled.div`
+    width: 99px;
+    height: 46px;
+    input[type="time" i]::-webkit-calendar-picker-indicator {
+        display: none;
+    }
+`;
+const StyledInput = styled.input`
+    width: 99px;
+    height: 46px;
+    border: 1.53px solid #d4d0d0;
+    border-radius: 8px;
+    font-size: 16px;
+    font-weight: 400;
+    font-family: "DINNextRoundedLTW01-Regular";
+    padding-left: 30px;
+`;
+const StyledText = styled.span`
+    width: 78px;
+    height: 36px;
+    font-family: "DINNextRoundedLTW04-Medium";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 18px;
+    display: flex;
+`;
 export default DataInput;
