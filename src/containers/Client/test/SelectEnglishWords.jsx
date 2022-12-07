5cab797f157b589d6310a8e0ac49214a2df3696f
Chika <chyngyzalkarbekov@gmail.com>
2022-12-07 16:42:00 +0600

    save

--- src/containers/Client/test/SelectEnglishWords.jsx
+++ src/containers/Client/test/SelectEnglishWords.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import ClientContainer from "components/UI/ClientContainer";
+import Progress from "components/UI/Progress";
 import UICard from "components/UI/UICard";
 
 function SelectEnglishWords() {
@@ -11,6 +12,7 @@ function SelectEnglishWords() {
                     cardWidth="900px"
                     cardBorderRadius="10px"
                     cardBoxShadow="0px 4px 39px -5px rgba(196, 196, 196, 0.6)">
+                    <Progress />
                     <div>
                         <p>select the real English words in this list</p>
                     </div>
