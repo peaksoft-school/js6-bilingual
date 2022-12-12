da791fe1437e3aba7130bb67da3ceb01de2e92b8
Chika <chyngyzalkarbekov@gmail.com>
2022-12-12 19:37:25 +0600

    start select main idea

--- src/containers/Client/test/ClientMainIdea.jsx
+++ src/containers/Client/test/ClientMainIdea.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function ClientMainIdea() {
-    return <div>ClientMainIdea</div>;
+    return (
+        <div>
+            <section>
+                <div>
+                    <h2>PASSAGE</h2>
+                    <hr />
+                    <p>
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                        doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                        veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                        ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                        consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                        porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+                        adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                        dolore magnam aliquam quaerat voluptatem.
+                    </p>
+                </div>
+                <div>
+                    <h1>Select the best title for the passage</h1>
+                    <div />
+                </div>
+            </section>
+        </div>
+    );
 }
